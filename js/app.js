0969e13646e9370bbca79bc0915425b8df8f7025
JONG-MIN BOO <v6306@naver.com>
2016-06-23 18:12:59 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,44 +49,18 @@ function full_screen(event)
 	}
 }
 
+$( document ).ready(function() { 
+       $('.btn-example').click(function(){ 
+        $('#layer2, #layer_body').show(); 
+        $('#layer2').css("top", Math.max(0, $(window).scrollTop() + 100) + "px"); 
+        // $('#popup_layer').css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px"); 
+    }); 
+    $('#olayer_body, .close').click(function(e){ 
+        e.preventDefault(); 
+        $('#layer2, #layer_body').hide(); 
+    }); 
+});
 
-$('.btn-example').click(function(){
-        var $href = $(this).attr('href');
-        layer_popup($href);
-    });
-    function layer_popup(el){
-
-        var $el = $(el);        //레이어의 id를 $el 변수에 저장
-        var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
-
-        isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
-
-        var $elWidth = ~~($el.outerWidth()),
-            $elHeight = ~~($el.outerHeight()),
-            docWidth = $(document).width(),
-            docHeight = $(document).height();
-
-        // 화면의 중앙에 레이어를 띄운다.
-        if ($elHeight < docHeight || $elWidth < docWidth) {
-            $el.css({
-                marginTop: -$elHeight /2,
-                marginLeft: -$elWidth/2
-            })
-        } else {
-            $el.css({top: 0, left: 0});
-        }
-
-        $el.find('a.btn-layerClose').click(function(){
-            isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-            return false;
-        });
-
-        $('.layer .dimBg').click(function(){
-            $('.dim-layer').fadeOut();
-            return false;
-        });
-
-    }
 
 function data_Load()
 {
